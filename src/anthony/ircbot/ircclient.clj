c8ec086d0e3ea610923f10ef6486209a8ba65651
Anthony Snavely <anthony.snavely@gmail.com>
2011-08-20 19:24:19 -0700

    tiny bit of cleanup

--- src/anthony/ircbot/ircclient.clj
+++ src/anthony/ircbot/ircclient.clj
@@ -3,7 +3,7 @@
     (java.io OutputStreamWriter BufferedWriter InputStreamReader BufferedReader))
   (:use [anthony.ircbot.irc]))
 
-(defn parse-message [event-functions connection-info operation line]
+(defn parse-message-fn [event-functions connection-info operation line]
   (let [keyword-operation (keyword operation)
         source (get-arg line 0)
         args-line (get-args-str line)]
@@ -16,16 +16,17 @@
         irc-input (BufferedReader. (InputStreamReader. (.getInputStream irc-sock)))
         irc-output (BufferedWriter. (OutputStreamWriter. (.getOutputStream irc-sock)))
         irc-server (:host conn-info)
-        connection-info (assoc conn-info :writer irc-output :sock irc-sock :reader irc-input)]
-    (write-line irc-output (str "NICK " (:nick conn-info)))
-    (write-line irc-output (str "USER " (:user conn-info) " * * :" (:version conn-info)))
-    (loop [irc-string (.readLine irc-input)]
-      (when (not= irc-string nil)
-        (println irc-string)
-        (if (= (.startsWith irc-string "PING") true)
-          (write-line irc-output (.replace irc-string "PING" "PONG")))
-        (if (= (.startsWith irc-string ":") true)
-          (let [irc-string-new (subs irc-string 1) operation (get-arg irc-string-new 1)]
-            (parse-message event-functions connection-info operation irc-string-new)))
-        (recur (.readLine irc-input)))))
-  (println "Connection was closed =("))
+        connection-info (assoc conn-info :writer irc-output :sock irc-sock :reader irc-input)
+        parse-message (fn [operation line] (parse-message-fn event-functions connection-info operation line))]
+      (write-line irc-output (str "NICK " (:nick conn-info)))
+      (write-line irc-output (str "USER " (:user conn-info) " * * :" (:version conn-info)))
+      (loop [irc-string (.readLine irc-input)]
+        (when (not= irc-string nil)
+          (println irc-string)
+          (if (= (.startsWith irc-string "PING") true)
+            (write-line irc-output (.replace irc-string "PING" "PONG")))
+          (if (= (.startsWith irc-string ":") true)
+            (let [irc-string-new (subs irc-string 1) operation (get-arg irc-string-new 1)]
+              (parse-message operation irc-string-new)))
+          (recur (.readLine irc-input)))))
+        (println "Connection was closed =("))
